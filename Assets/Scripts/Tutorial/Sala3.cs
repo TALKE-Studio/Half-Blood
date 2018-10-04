09c636be3ac5f7774cee18ac73df2e4d1a01091e
Logon Aluno <logonrmlocal@fiap.com.br>
2018-10-04 13:36:14 -0300

    tutorial

---
+++ Assets/Scripts/Tutorial/Sala3.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Sala3 : MonoBehaviour {
+	GameObject pedra;
+	GameObject pedrainv;
+	GameObject porta;
+	bool coletoupedra = false;
+	bool abriuporta = false;
+	// Use this for initialization
+	void Start () {
+		pedra = GameObject.FindGameObjectWithTag("PedraVerde");
+		pedrainv = GameObject.FindGameObjectWithTag("PedraVerdeInv");
+		porta = GameObject.FindGameObjectWithTag("PortaFase4");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerStay(Collider other){
+		if(other.gameObject.tag == "PedraVerde" && Input.GetKeyDown (KeyCode.Space) == true){
+			coletoupedra = true;
+			print("Moco Colidiu aqui");
+			gameObject.GetComponent<Animator>().SetBool("PegouChao", true);
+			StartCoroutine(PegandoPedra());
+			StartCoroutine (TerminarAnim());
+		}
+		if(other.gameObject.tag == "GarraTutorial" && Input.GetKeyDown (KeyCode.Space) == true){
+			if(coletoupedra == true){
+				gameObject.GetComponent<Animator>().SetBool("TocouParede", true);
+				abriuporta = true;
+				StartCoroutine(AbrindoPorta());
+				StartCoroutine(TocandoParede());
+				StartCoroutine(TerminarAnim());
+			}
+
+			
+		}
+	}
+
+	IEnumerator TocandoParede(){
+		yield return new WaitForSeconds(1f);
+		pedrainv.GetComponent<MeshRenderer>().enabled = true;
+	}
+
+	IEnumerator AbrindoPorta(){
+		yield return new WaitForSeconds(2f);
+		porta.GetComponent<Animator>().SetBool("Abrir", true);
+	}
+
+	IEnumerator PegandoPedra(){
+		yield return new WaitForSeconds(1f);
+		pedra.GetComponent<MeshRenderer>().enabled = false;
+	}
+
+	IEnumerator TerminarAnim(){
+		yield return new WaitForSeconds(0.6f);
+		gameObject.GetComponent<Animator>().SetBool("TocouParede", false);
+		gameObject.GetComponent<Animator>().SetBool("PegouChao", false);
+	}
+}
