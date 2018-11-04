05e1d398223ff27cdfe0e3ffef5388f1cf621b7b
tiago dias <tiago.diassantos@hotmail.com>
2018-11-04 01:02:10 -0200

    Texto_Tutorial_Finalizado

---
+++ Assets/Scripts/Tutorial/CutScene/AnimacaoTexto/ScriptsCanvas/TextoFinal.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextoFinal : MonoBehaviour {
+
+    public GameObject Texto9;
+    public GameObject textoContinuar;
+    bool podeavancar = false;
+    Touch touch;
+    static public bool tocou = false;
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(Trava());
+        StartCoroutine(Continuar());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            if ((Input.GetKeyDown(KeyCode.Space) || Input.GetTouch(i).phase == TouchPhase.Began || Input.GetMouseButtonDown(0)) && podeavancar == true)
+            {
+                Texto9.GetComponent<Animator>().SetBool("Proximo", true);
+                textoContinuar.GetComponent<Animator>().SetBool("Proximo", true);
+                tocou = true;
+                
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && podeavancar == true)
+        {
+            Texto9.GetComponent<Animator>().SetBool("Proximo", true);
+            textoContinuar.GetComponent<Animator>().SetBool("Proximo", true);
+            tocou = true;
+
+        }
+
+    }
+
+    IEnumerator Trava()
+    {
+        yield return new WaitForSeconds(6f);
+        podeavancar = true;
+
+    }
+
+    IEnumerator Continuar()
+    {
+        yield return new WaitForSeconds(5f);
+        textoContinuar.SetActive(true);
+    }
+
+
+}
--- Assets/Scripts/Tutorial/CutScene/FimTutorial.cs
+++ Assets/Scripts/Tutorial/CutScene/FimTutorial.cs
@@ -7,22 +7,48 @@ public class FimTutorial : MonoBehaviour {
 	public GameObject Texto_Tutorial;
 	public GameObject Imagem_Tutorial;
 	public GameObject Tela_Final;
+    public GameObject Tela_TextoFinal;
+    public GameObject Texto8;
+    public GameObject Texto9;
+    public GameObject continuar;
+    bool podeavancar = false;
+    Touch touch;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            if ((Input.GetKeyDown(KeyCode.Space) || Input.GetTouch(i).phase == TouchPhase.Began || Input.GetMouseButtonDown(0)) && podeavancar == true)
+            {
+                Texto8.GetComponent<Animator>().SetBool("Proximo", true);
+                continuar.GetComponent<Animator>().SetBool("Proximo", true);
+                StartCoroutine(IrTexto9());
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && podeavancar == true)
+        {
+            Texto8.GetComponent<Animator>().SetBool("Proximo", true);
+            continuar.GetComponent<Animator>().SetBool("Proximo", true);
+            StartCoroutine(IrTexto9());
+        }
+        if(TextoFinal.tocou == true)
+        {
+            StartCoroutine(TelaBranca());
+        }
+    }
 
 	void OnTriggerEnter(Collider other){
 		if(other.gameObject.tag == "Player"){
 			StartCoroutine(Conquista());
-			StartCoroutine(TelaBranca());
-		}
+            StartCoroutine(TelaPretaFinal());
+            StartCoroutine(Texto8Aparecer());
+            StartCoroutine(Trava());
+        }
 	}
 
 	IEnumerator Conquista(){
@@ -30,8 +56,44 @@ public class FimTutorial : MonoBehaviour {
 		Texto_Tutorial.SetActive(true);
 		Imagem_Tutorial.SetActive(true);
 	}
-	IEnumerator TelaBranca(){
-		yield return new WaitForSeconds(14f);
-        GameObject.FindGameObjectWithTag("TelaBranca").GetComponent<Animator>().SetTrigger("gameOver");
+
+    IEnumerator TelaPretaFinal()
+    {
+        yield return new WaitForSeconds(14f);
+        Tela_TextoFinal.SetActive(true);
+    }
+
+    IEnumerator Texto8Aparecer()
+    {
+        yield return new WaitForSeconds(15f);
+        Texto8.SetActive(true);
+        StartCoroutine(Continuar());
+
+    }
+
+    IEnumerator Continuar()
+    {
+        yield return new WaitForSeconds(5f);
+        continuar.SetActive(true);
+    }
+
+    IEnumerator Trava()
+    {
+        yield return new WaitForSeconds(20f);
+        podeavancar = true;
+
+    }
+    IEnumerator IrTexto9()
+    {
+        yield return new WaitForSeconds(2f);
+        continuar.SetActive(false);
+        podeavancar = false;
+        Texto9.SetActive(true);
+        Destroy(Texto8.gameObject);
+    }
+
+    IEnumerator TelaBranca(){
+		yield return new WaitForSeconds(1.5f);
+        GameObject.FindGameObjectWithTag("TelaBrancaTutorial").GetComponent<Animator>().SetTrigger("gameOver");
     }
 }
